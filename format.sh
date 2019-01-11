a72b64a322479de8b98d6c8951885b7de42422c1
Joseph Espy <jespy@gwu.edu>
2019-01-11 12:55:24 -0500

    rewrote format.sh script to be more robust and run formatter and linter

--- format.sh
+++ format.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-make clean && find . -type f | egrep '\.(h|c(pp)?)$' |  xargs clang-format -style='google' -i
-find . -type f | egrep '\.(h|cpp)$' |  xargs python cpplint.py
 
+make clean 
 
+# find reports all of the paths other than git and googletest
+# the grep regex finds .h, .cpp and .c files
+# and xargs puts it all into clang format, using google style guide
+find . -type f -not -path "./googletest/*" -not -path "./.git/*" \
+	| egrep '\.(h|c(pp)?)$' \
+	|  xargs clang-format -style='google' -i
+
+# Same process to run cpplint, over only cpp files
+ find . -type f -not -path "./googletest/*" -not -path "./.git/*" \
+	| egrep '\.(cpp|h|cc)$' \
+	|  xargs python cpplint.py
